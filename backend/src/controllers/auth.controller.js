98ef2fd1abf42813cf3c81aa8fad66552146bd92
vaibhav <vibhuuu05@gmail.com>
2025-05-18 20:27:44 +0530

    mongodb added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,11 @@
-export function signup(req, res) {
+export  async function signup(req, res) {
     res.send("signup");
+}
+
+export  async function login(req, res) {
+    res.send("login");
+}
+
+export  async function logout(req, res) {
+    res.send("logout");
 }
