3a9a2b92216ee6da73d5f9ce226cadc76c6b16b9
vaibhav <vibhuuu05@gmail.com>
2025-05-19 15:45:00 +0530

    all pages added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,26 @@
-import Login from './Login.jsx';
-import './App.css';
+import { Routes,Route } from "react-router";  
+import HomePage from "../src/pages/HomePage.jsx";
+import LoginPage from "../src/pages/LoginPage.jsx";
+import SignUpPage from "../src/pages/SignUpPage.jsx";
+import OnBoardingPage from "../src/pages/OnBoardingPage.jsx";
+import NotificationPage from "../src/pages/NotificationPage.jsx";
+import ChatPage from "../src/pages/ChatPage.jsx";
+import CallPage from "../src/pages/CallPage.jsx";
 
-function App() {
-  return (
+  const App =()=>{
+    return(
     <div>
-      <h1>Login</h1>
-      <Login />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/onboarding" element={<OnBoardingPage />} />
+        <Route path="/notification" element={<NotificationPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/call" element={<CallPage />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
